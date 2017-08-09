7526ccb4a9682b6890ff8c9f7014e04b830a6875
Ruslan <rus-march@yandex.ua>
2017-08-09 13:10:41 +0300

    middleware Properties

--- src/AppBundle/Controller/PropertiesController.php
+++ src/AppBundle/Controller/PropertiesController.php
@@ -19,94 +19,133 @@ use AppBundle\Entity\User;
 class PropertiesController extends FOSRestController
 {
 
-    function token(Request $request)
+    /**
+     * @return array
+     */
+    public function indexAction(Request $request)
     {
         $token = $request->headers->get('X-AUTH-TOKEN');
-        $user_id = $request->headers->get (' X-USER-ID');
-
+        $user_id = $request->headers->get ('X-USER-ID');
         $em = $this->getDoctrine()->getManager();
         $reposytory = $em->getRepository('AppBundle:User');
         $user = $reposytory->findOneBy(['token'=>$token, 'user_id'=>$user_id]);
 
-        if(!$user){
-            return new View(["message" => "Invalid token or user's private key"], Response::HTTP_NOT_ACCEPTABLE);
+        if($user){
+            $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->findAll();
+            if ($properties === null) {
+                return ['message' => 'there are no Propertie exist'];
+            }
+            return ['data' => $properties];
+        }else{
+
+            return new View(["message" => "Mismatches of the token or the user's private key"], Response::HTTP_NOT_ACCEPTABLE);
         }
+
 
     }
 
 
-    /**
-     * @return array
-     */
-    public function indexAction()
+    public function showAction($id, Request $request)
     {
+        $token = $request->headers->get('X-AUTH-TOKEN');
+        $user_id = $request->headers->get ('X-USER-ID');
+        $em = $this->getDoctrine()->getManager();
+        $reposytory = $em->getRepository('AppBundle:User');
+        $user = $reposytory->findOneBy(['token'=>$token, 'user_id'=>$user_id]);
 
-        $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->findAll();
-        if ($properties === null) {
-            return ['message' => 'there are no Propertie exist'];
+        if($user){
+            $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
+            if ($properties === null) {
+                return ['message' => ' Propertie not found'];
+            }
+            return ['data' => $properties];
+        }else{
+            return new View(["message" => "Mismatches of the token or the user's private key"], Response::HTTP_NOT_ACCEPTABLE);
         }
-        return ['data' => $properties];
-    }
 
 
-    public function showAction($id)
-    {
-        $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
-        if ($properties === null) {
-            return ['message' => ' Propertie not found'];
-        }
-        return ['data' => $properties];
     }
 
 
     public function createAction(Request $request)
     {
 
-        $body = $request->request->all();
-        $properties = new Propertie();
-        $form = $this->createForm(PropertieType::class, $properties, [
-            'csrf_protection' => false,
-        ]);
-        $form->submit($body);
-
+        $token = $request->headers->get('X-AUTH-TOKEN');
+        $user_id = $request->headers->get ('X-USER-ID');
         $em = $this->getDoctrine()->getManager();
-        $em->persist($properties);
-        $em->flush();
+        $reposytory = $em->getRepository('AppBundle:User');
+        $user = $reposytory->findOneBy(['token'=>$token, 'user_id'=>$user_id]);
 
-        return ['data' => $properties];
+        if($user){
+            $body = $request->request->all();
+            $properties = new Propertie();
+            $form = $this->createForm(PropertieType::class, $properties, [
+                'csrf_protection' => false,
+            ]);
+            $form->submit($body);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($properties);
+            $em->flush();
+
+            return ['data' => $properties];
+        }else{
+            return new View(["message" => "Mismatches of the token or the user's private key"], Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+
     }
 
 
     public function updateAction($id, Request $request)
     {
-        $body = $request->request->all();
-        $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
-        if ($properties) {
-            $form = $this->createForm(PropertieType::class, $properties, [
-                'csrf_protection' => false,
-            ]);
-            $form->submit($body);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($properties);
-            $em->flush();
-            return new View(["message" => "Propertie Updated Successfully"], Response::HTTP_OK);
-        } else {
-            return new View(["message" => "Properties  cannot be empty"], Response::HTTP_NOT_ACCEPTABLE);
+        $token = $request->headers->get('X-AUTH-TOKEN');
+        $user_id = $request->headers->get ('X-USER-ID');
+        $em = $this->getDoctrine()->getManager();
+        $reposytory = $em->getRepository('AppBundle:User');
+        $user = $reposytory->findOneBy(['token'=>$token, 'user_id'=>$user_id]);
+        if($user){
+            $body = $request->request->all();
+            $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
+            if ($properties) {
+                $form = $this->createForm(PropertieType::class, $properties, [
+                    'csrf_protection' => false,
+                ]);
+                $form->submit($body);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($properties);
+                $em->flush();
+                return new View(["message" => "Propertie Updated Successfully"], Response::HTTP_OK);
+            } else {
+                return new View(["message" => "Properties  cannot be empty"], Response::HTTP_NOT_ACCEPTABLE);
+            }
+        }else{
+            return new View(["message" => "Mismatches of the token or the user's private key"], Response::HTTP_NOT_ACCEPTABLE);
         }
+
     }
 
 
-    public function deleteAction($id)
+    public function deleteAction($id, Request $request)
     {
-        $sn = $this->getDoctrine()->getManager();
-        $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
-        if (!$properties) {
-            return new View("user not found", Response::HTTP_NOT_FOUND);
-        } else {
-            $sn->remove($properties);
-            $sn->flush();
+        $token = $request->headers->get('X-AUTH-TOKEN');
+        $user_id = $request->headers->get ('X-USER-ID');
+        $em = $this->getDoctrine()->getManager();
+        $reposytory = $em->getRepository('AppBundle:User');
+        $user = $reposytory->findOneBy(['token'=>$token, 'user_id'=>$user_id]);
+        if($user){
+            $sn = $this->getDoctrine()->getManager();
+            $properties = $this->getDoctrine()->getRepository('AppBundle:Propertie')->find($id);
+            if (!$properties) {
+                return new View("property not found", Response::HTTP_NOT_FOUND);
+            } else {
+                $sn->remove($properties);
+                $sn->flush();
+            }
+            return new View("deleted successfully", Response::HTTP_OK);
+        }else{
+            return new View(["message" => "Mismatches of the token or the user's private key"], Response::HTTP_NOT_ACCEPTABLE);
         }
-        return new View("deleted successfully", Response::HTTP_OK);
     }
 
 
